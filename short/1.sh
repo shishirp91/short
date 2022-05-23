92ee6c210f07d5aeb58ef44159e353ce61da7ea0
shishirp91 <shishirpatiL@icloud.com>
2022-05-23 16:12:15 +0530

    final:

--- short/1.sh
+++ short/1.sh
@@ -1,7 +1,16 @@
-# Run this as ./1.sh and not using bash
+# Please su into root and run this as ./1.sh and not using bash.
+# Run this script on both master and worker nodes.
 
 #!/bin/bash
 read -p "Enter your master-node's IP address : " IP1 
 export MIP=$IP1
 read -p "Enter your worker-node's IP address : " IP2
 export WIP=$IP2 
+sed -i "1i $MIP kubernetes-master.learnitguide.net kubernetes-master" /etc/hosts11
+sed -i "2i $WIP kubernetes-worker1.learnitguide.net kubernetes-worker1" /etc/hosts11
+sudo swapoff -a
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00 docker.io
+sudo systemctl start docker && sudo systemctl enable docker
--- short/2.sh
+++ short/2.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-  
-sudo echo '"echo $MIP" kubernetes-master.learnitguide.net kubernetes-master' >> /etc/hosts11
-sudo echo 'echo "$WIP" kubernetes-worker1.learnitguide.net kubernetes-worker1' >> /etc/hosts11
+#Don't run this as root, run it as normal user.
+
+sudo kubeadm init  --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+sudo iptables -F
+
