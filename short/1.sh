bb533c4a8ee07004bf977a26ac859f8ce836bce8
shishirp91 <shishirpatiL@icloud.com>
2022-05-23 13:16:51 +0530

    added

---
+++ short/1.sh
@@ -0,0 +1,8 @@
+# Run this as ./1.sh and not using bash
+
+#!/bin/bash
+read -p "Enter your master-node's IP address : " IP1 
+export MIP=$IP1
+read -p "Enter your worker-node's IP address : " IP2
+export WIP=$IP2
+sudo echo $WIP > x 
---
+++ short/2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+  
+$(date) kubernetes-master.learnitguide.net kubernetes-master
+$WIP kubernetes-worker1.learnitguide.net kubernetes-worker1
+EOF
